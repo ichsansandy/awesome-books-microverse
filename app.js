728eabac349250e1e636dbd477959950cac91e73
rbd3 <rabedesanaa@gmail.com>
2023-05-30 11:07:39 +0300

    Add image

--- app.js
+++ app.js
@@ -39,11 +39,11 @@ class BookStore {
         instagramLink: 'https://instagram.com/)_olaniee__',
       },
       {
-        name: 'Ichsan Sandy',
-        classImage: 'ichsan-img',
-        githubLink: 'https://github.com/ichsansandy',
-        linkedLink: 'https://www.linkedin.com/in/ichsans/',
-        instagramLink: 'https://instagram.com/ichsans__',
+        name: 'Andry Narson',
+        classImage: 'Andry-img',
+        githubLink: 'https://github.com/rbd3',
+        linkedLink: 'https://linkedin.com/in/andry-narson-rabedesana-15b8b4248',
+        instagramLink: 'https://twitter.com/@Narson321',
       },
     ];
   }
